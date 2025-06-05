bad987f865ba7108af33b307ba124a53d3e6200b
maymisson <maymisson@gmail.com>
2025-06-04 21:31:46 -0300

    control deploy using branch refs (#19)
    * added FP_A
    
    * added FP_A
    
    * fix upns ID
    
    * added black theme
    
    * alter theme
    
    * added FP_A
    
    * added FP_A
    
    * fix upns ID
    
    * added black theme
    
    * inovate FP_A
    
    * alter theme
    
    * added action deploy all  (#6)
    
    * added FP_A
    
    * added FP_A
    
    * fix upns ID
    
    * added black theme
    
    * inovate FP_A
    
    * alter theme
    
    * deploy all module unique test
    
    * test deploy all modify theme (#7)
    
    * Feature 2 (#8)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * Feature 2
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refatoring
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * deploy workspace
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * Feature X (#13)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * added workflow new
    
    * Feature 2 (#14)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * added workflow new
    
    * added workflow new
    
    * cleaning repo (#15)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * added workflow new
    
    * added workflow new
    
    * cleaning repo
    
    * clear repo (#17)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * added workflow new
    
    * added workflow new
    
    * cleaning repo
    
    * cleaning repo
    
    * control deploy using branch refs (#18)
    
    * test deploy all modify theme
    
    * fixing deploy all
    
    * added logo
    
    * refactoring module pipe
    
    * resolve conflicts
    
    * alter directory_repository
    
    * added workflow new
    
    * added workflow new
    
    * added workflow new
    
    * cleaning repo
    
    * cleaning repo
    
    * control deploy using branch refs

--- deploy_workspace.py
+++ deploy_workspace.py
@@ -7,17 +7,10 @@ root_directory = Path(__file__).resolve().parent
 
 # Parameters for the FabricWorkspace Connection
 workspace_id = os.getenv("FABRIC_WORKSPACE_ID")
-
 repository_directory = os.path.join(os.getenv("GITHUB_WORKSPACE", "."), "src")
-
 client_id = os.getenv("FABRIC_CLIENT_ID")
 client_secret = os.getenv("FABRIC_CLIENT_SECRET")
 tenant_id = os.getenv("FABRIC_TENANT_ID")
-
-print(f"Client ID: {client_id}")
-print(f"Client Secret: {client_secret}")
-print(f"Tenant ID: {tenant_id}")
-
 token_credential = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
 
 # Supported Item Types fabric-cicd
